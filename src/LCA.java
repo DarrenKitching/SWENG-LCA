83e354c4ac597c0797d7bedb9c35e151397c0ec0
Darren Kitching <darrenkitching98@gmail.com>
2019-09-25 11:14:37 +0100

    Null input tests

--- src/LCA.java
+++ src/LCA.java
@@ -1,5 +1,22 @@
 class LCA {
-    public static void main(String[] args) {
+    public static class Node {
+        int value;
+        Node left;
+        Node right;
+
+        Node(int value) {
+            this.value = value;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static Node findLCA(Node nodeOne, Node nodeTwo) {
+
+        return null;
+    }
+
+    public static void main(String args[]) {
 
     }
 }
--- src/LCAtest.java
+++ src/LCAtest.java
@@ -6,7 +6,11 @@ import org.junit.runners.JUnit4;
 @RunWith(JUnit4.class)
 public class LCAtest {
     @Test
-    public void testConstructor() {
-
+    public void testNull() {
+        LCA lca = new LCA();
+        assertEquals(lca.findLCA(null, null), null); // Two null inputs
+        LCA.Node test = new LCA.Node(5);
+        assertEquals(lca.findLCA(null, test), null); // First input null
+        assertEquals(lca.findLCA(test, null), null); // Second input null
     }
 }
